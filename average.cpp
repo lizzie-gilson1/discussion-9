682d6a0e939a1a6898b502bdd35877f582ee718a
lizzie-gilson1 <160568310+lizzie-gilson1@users.noreply.github.com>
2025-11-06 15:07:50 -0800

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -2,16 +2,21 @@
 #include <vector>
 
 double average(const std::vector<int>& nums) {
-    double sum;
-    for (size_t i = 0; i <= nums.size(); ++i) {
-        sum += nums[i];
+    // function to find the average a vector of ints
+    double* sum = new double(0.0);
+    for (size_t i = 0; i <= nums.size(); i++) {
+        *sum += nums[i];  
     }
-    return sum / nums.size();
+
+    result = *sum / nums.size();
+    return result; 
 }
 
 int main() {
-    std::vector<int> data = {1, 2, 3, 4, 5};
-    double avg = average(data);
+    std::vector<int>* data = new std::vector<int>{1, 2, 3, 4, 5};
+    delete data;
+    data = nullptr;
+    double avg = average(*data);
     std::cout << "Average: " << avg << std::endl;
     return 0;
 }
